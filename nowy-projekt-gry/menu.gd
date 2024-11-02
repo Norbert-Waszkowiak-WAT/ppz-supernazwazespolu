61def62ff0716ae8d903649c275054114c228741
MichalG13 <grzedamichal2@gmail.com>
2024-11-02 17:09:46 +0100

    menu
    zmieniłem ułożenie przycisków i dodałem jeszcze raz scenę gry

--- nowy-projekt-gry/menu.gd
+++ nowy-projekt-gry/menu.gd
@@ -6,7 +6,7 @@ func _on_graj_pressed() -> void:
 
 
 func _on_opcje_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://opcje_menu.tscn")
 
 
 func _on_wyjdź_pressed() -> void:
