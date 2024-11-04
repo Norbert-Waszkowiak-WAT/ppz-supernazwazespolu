4a16a915ae5484e52e13c39586c536e17b15c06d
MichalG13 <grzedamichal2@gmail.com>
2024-11-04 10:19:04 +0100

    zmiany

--- nowy-projekt-gry/wyborlvl.gd
+++ nowy-projekt-gry/wyborlvl.gd
@@ -3,7 +3,5 @@ extends Control
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://gra.tscn")
 
-
-
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scena3d.tscn")
