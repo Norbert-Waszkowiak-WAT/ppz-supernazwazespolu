320b4bafc3a2323b0f77181ab68501b2357bc558
OliwiaM13 <oliwiamikucka@gazeta.pl>
2024-11-18 09:11:26 +0100

    Przycisk powrotu do menu

--- nowy-projekt-gry/wyborlvl.gd
+++ nowy-projekt-gry/wyborlvl.gd
@@ -2,3 +2,8 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://gra.tscn")
+
+
+
+func _on_powrót_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
