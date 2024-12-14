e3e4af02b572d3c74fe36a2cf4827e594f655c8e
MichalG13 <grzedamichal2@gmail.com>
2024-12-14 21:37:07 +0100

    wyglad menu

--- nowy-projekt-gry/menu.gd
+++ nowy-projekt-gry/menu.gd
@@ -5,9 +5,9 @@ func _on_graj_pressed() -> void:
 	get_tree().change_scene_to_file("res://wyborlvl.tscn")
 
 
-func _on_opcje_pressed() -> void:
+func _on_graj_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://opcje_ustawien.tscn")
 
 
-func _on_wyjdź_pressed() -> void:
+func _on_graj_3_pressed() -> void:
 	get_tree().quit()
--- nowy-projekt-gry/wyborlvl.gd
+++ nowy-projekt-gry/wyborlvl.gd
@@ -1,9 +1,9 @@
 extends Control
 
-func _on_button_pressed() -> void:
+func _on_a_pressed() -> void:
 	get_tree().change_scene_to_file("res://gra.tscn")
 
 
 
-func _on_powrót_pressed() -> void:
+func _on_a_6_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
