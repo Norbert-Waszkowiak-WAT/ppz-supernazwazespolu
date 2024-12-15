f6872f78c0b9c1162daeb35ec15f5458299a0f86
MichalG13 <grzedamichal2@gmail.com>
2024-12-15 10:28:43 +0100

    zmiana wyglądu menu

--- nowy-projekt-gry/wyborlvl.gd
+++ nowy-projekt-gry/wyborlvl.gd
@@ -1,9 +1,11 @@
 extends Control
 
-func _on_a_pressed() -> void:
+
+
+func _on_graj_pressed() -> void:
 	get_tree().change_scene_to_file("res://gra.tscn")
 
 
 
-func _on_a_6_pressed() -> void:
+func _on_graj_6_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
