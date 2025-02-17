0b003702e954032a2bfbff38c689c1ed452926dd
OliwiaM13 <oliwiamikucka@gazeta.pl>
2025-02-17 10:36:14 +0100

    d

--- nowy-projekt-gry/gra.gd
+++ nowy-projekt-gry/gra.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://wyborlvl.tscn")
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
